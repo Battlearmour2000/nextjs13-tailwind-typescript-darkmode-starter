f38411695bf4c61359166b4ce45b0d85fff282f2
Vicent Simon <67832502+RonnieTaz@users.noreply.github.com>
2023-08-25 14:47:59 +0300

    :constructuion: feat(Hero): Added static props fetcher for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
 import Hero from "../sections/Hero";
+import type { THero } from '../sections/Hero';
 
-export default function Home() {
+export default function Home({ hero }) {
   return (
     <>
       <div>
-        <Hero />
+        <Hero data={hero} />
       </div>
     </>
   );
 }
+
+// Use this function on pages to fetch data from server at build time and pass on to
+// the Page component
+// https://nextjs.org/docs/pages/building-your-application/data-fetching/get-static-props
+export const getStaticProps: GetStaticProps<{ hero: THero[] }> = async () => {
+  //const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res = await fetch('https://jsonplaceholder.typicode.com/photos')
+  const data = await res.json();
+  return { 
+    props: {
+      hero: data
+    }
+  }
+} 
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -2,49 +2,37 @@ import Image from "next/image";
 import Button from "../components/Button";
 import iphone from "../public/iphone-13.jpeg";
 import React from "react";
+import type { GetStaticProps } from 'next';
 
 // https://tailwindcss.com/docs/installation
 
 // run npm install
 
 //  create the component for the product details
-export 
 
-const Hero = ({}) => {
-//hero var is initialized
-//Type structure of items inside the array
-type heroType ={
+export type THero = {
   id: number,
   //name: string,
   thumbnailUrl: string,
 } 
-//set up the types
-const [hero, setHero] = React.useState<heroType[]>([])
 
-const getStaticProps = async() => {
-  //const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const res = await fetch('https://jsonplaceholder.typicode.com/photos')
-  const data = await res.json();
-  const hero = data.slice(0, 3);
-  //gets data from the json, runs at build time 
-  setHero(hero)
-}  
-React.useEffect(()=>{
-    getStaticProps()
-  }, [])
-  
+type HeroProps = {
+  data: THero[]
+}
+
+const Hero = ({ data }: HeroProps) => {  
   return (
     <div className="mx-auto">
       <Image src={iphone} 
         alt="iphone" 
         width={200}
-        mx-auto />
+        className='mx-auto' />
       <h1 className="text-black text-4xl font-bold pt-5 pb-5"> iPhone 13</h1>
       <p className="text-black text-1xl pt-5 pb-5"> 6.1-inch Super Retina XDR display,
       A curved notched design with a rounded edge and vibrant color.</p>
       <p>One year warranty included and free delivery within Dar-es-salaam</p>
       <h2 className="text-black text-2xl font-bold pt-5 pb-5"> Tshs 2,000,000</h2>
-      {hero.map(hero=> {
+      {data.map(hero=> {
         console.log(hero)
         return(
         
